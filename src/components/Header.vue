2c941ce4ace0923d3d9bcd683592240591e52814
G-Salis <giuseppesalislavoro@gmail.com>
2021-12-14 14:27:10 +0100

    Header Completed

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,81 @@
 <template>
-   <section class="header">
 
-     Header
+<section class="header">
 
-  </section>
+  <div class="hdr">
+
+
+       <div class="ctn-left">
+
+          <div class="hdr-social">
+            <i class="fab fa-facebook-f"></i>
+            <i class="fab fa-twitter"></i>
+            <i class="fab fa-instagram"></i>
+            <i class="fab fa-youtube"></i>
+          </div>
+
+       <span>Shopping Card</span>
+
+      </div>
+ 
+       
+     
+     <div class="ctn-center">
+
+       <div class="hdr-account">
+          My Account <i class="fas fa-chevron-down"></i>
+       </div>
+
+     </div>
+
+     <div class="ctn-right">
+
+       <div class="hdr-cart">
+          <i class="fas fa-shopping-cart"></i> CART <i class="fas fa-chevron-down"></i>
+       </div>
+
+     </div>
+
+     
+
+  </div>
+
+  <div class="hdr">
+
+
+       <div class="ctn-logo">
+
+          <div class="hdr-img">
+            <img src="../assets/img/classic_shop_logo1x.png" alt="">
+          </div>
+
+      </div>     
+
+        <div class="ctn-nav">
+
+          <div class="hdr-list">
+
+            <span>Home<i class="fas fa-chevron-down"></i></span>
+            <span>Shop<i class="fas fa-chevron-down"></i></span>
+            <span>Product<i class="fas fa-chevron-down"></i></span>
+            <span>Categories<i class="fas fa-chevron-down"></i></span>
+            <span>News</span>
+            <span>Elements<i class="fas fa-chevron-down"></i></span>
+            <span class="btn-blue">SHOP NOW!</span>
+            <span><i class="fas fa-search"></i></span>
+            
+          </div>
+
+      </div> 
+
+  </div>
+
+</section>
+
+   
+
+  
+  
 </template>
 
 <script>
@@ -16,9 +88,83 @@ export default {
 </script>
 
 <style lang="scss">
-.header{
-  background-color: aqua;
-  height: 200px;
-  width: 100%;
+i{
+  font-size: 12px;
+  padding-left: 5px;
 }
+.hdr{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #64686C;
+  border-bottom: 1px solid #A0A5A5;
+  height: 80px;
+  
+}
+
+.ctn-left{
+  display: flex;
+  justify-content: space-between;
+  width: 60%;
+  margin-left: 7.5%;
+  height: 80px;
+  line-height: 80px;
+
+ 
+
+  span{
+    text-align: right;
+    padding-right: 5px;
+  }
+  
+}
+
+.hdr-social{
+  display: inline-block;
+  text-align: left;
+  
+}
+
+
+.hdr-social i{
+  margin-right: 20px;
+  font-size: 20px;
+}
+
+.ctn-center, .ctn-right{
+  width: 15%;
+  height: 80px;
+  line-height: 80px;
+}
+
+
+.ctn-center, .ctn-left{
+  border-right: 1px solid #A0A5A5;
+}
+
+.ctn-right{
+  margin-right: 7.5%;
+}
+
+
+p, span, .hdr-account, .hdr-cart{
+  font-weight: 900;
+}
+
+
+.ctn-logo{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-left: 7.5%;
+  height: 80px;
+}
+
+
+.hdr-list span{
+  padding: 5px 15px;
+}
+
+
+  @import "../assets/style/utilities.scss";
 </style>
