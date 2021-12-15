6ec4395a795c3e738b1b51572afd2beb8b783c06
G-Salis <giuseppesalislavoro@gmail.com>
2021-12-15 21:40:39 +0100

    Feedback completed

--- src/components/Feedback.vue
+++ src/components/Feedback.vue
@@ -1,7 +1,38 @@
 <template>
    <section class="feedback">
 
-     Feedback
+     <div class="fb-photo">
+       <img src="../assets/img/man_testimonial.png" alt="">
+     </div>
+
+     <div class="fb-text">
+       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt dicta nulla ipsa incidunt molestiae repudiandae enim, earum quam distinctio quaerat, id, excepturi labore hic? Sit fugit voluptatum provident officia ipsa.
+     </div>
+
+     <div class="fb-gen">
+
+       <span class="fb-name">
+        Dario Pineda,
+       </span>
+
+       <span class="fb-acquist">
+        Theme Fusion
+       </span>
+       
+     </div>
+
+     <div class="fb-gen">
+
+       <span class="fb-name">
+        <i class="fas fa-circle"></i>
+       </span>
+
+       <span class="fb-acquist">
+        <i class="far fa-circle"></i>
+       </span>
+       
+     </div>
+     
 
   </section>
 </template>
@@ -17,8 +48,36 @@ export default {
 
 <style lang="scss">
 .feedback{
-  background-color: rgb(216, 169, 169);
-  height: 200px;
+  background-image: url('../assets/img/testimonials_home_1_bg.jpg');
+  background-position: center;
+  background-size: contain;
+  height: 500px;
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  color: white;
+}
+
+.fb-photo{
+  width: 60px;
+  img{
+    width: 100%;
+  }
+}
+
+.fb-text{
+  width: 65%;
+  font-style: oblique;
+}
+
+.fb-gen{
+  margin: 30px;
+}
+
+.fb-acquist{
+  font-weight: 400;
 }
 </style>
